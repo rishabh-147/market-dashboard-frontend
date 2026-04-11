c8b25c7097d2d4d7ada24d667b0acfa3f54767cf
rishabh-147 <onlinestuff.rishabh@gmail.com>
2026-04-11 23:58:14 +0530

    [DEV] Changes for github pages

--- src/services/stockServices.js
+++ src/services/stockServices.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8080/v1/stock";
+// const BASE_URL = "http://localhost:8080/v1/stock";
+const BASE_URL = "https://market-dashboard-backend-h3i2.onrender.com/v1/stock";
 
 export const searchSymbol = (query) => {
   console.log("Searching for..." + query);
